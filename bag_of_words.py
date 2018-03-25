1ae7da69c8f604f8a475a3b972975a6e6d2436cb
Dawid <dawid@dawid.pl>
2018-03-25 05:41:19 +0100

    add surf feature

--- bag_of_words.py
+++ bag_of_words.py
@@ -6,6 +6,13 @@ import matplotlib.pyplot as plt
 import os
 import pathlib
 import _pickle as pickle
+from enum import Enum
+
+
+class Algorithm(Enum):
+
+	SWIFT = "SWIFT"
+	SURF = "SURF"
 
 
 def create_dirs(datasets_path, folder_name):
@@ -23,29 +30,49 @@ def create_dirs(datasets_path, folder_name):
 
 
 
-def create_bow(directory, df):
+def create_bow(directory, df, algorithm=Algorithm.SWIFT, value=None):
+
+	if value is None:
+		value = 0 if algorithm == Algorithm.SWIFT else 100
 
 	# initial data
 	data_size = df.shape[0]
 	bow = cv2.BOWKMeansTrainer(data_size)
 	labels = df['label'].values.tolist()
 
+	i = 0
+
 	for index, row in df.iterrows():
 
-		img_path = os.path.join(directory, row[0])
+		file_name = row[0]
+
+		if int(os.path.splitext(file_name)[0]) % 7 != 0:
+			continue
+
+		img_path = os.path.join(directory, file_name)
 
 		img = cv2.imread(img_path)
 		gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
-		sift = cv2.xfeatures2d.SIFT_create()
+		if algorithm == Algorithm.SWIFT:
+			sift = cv2.xfeatures2d.SIFT_create(value)
+			kp, des = sift.detectAndCompute(gray, None)
+
+		else:
+			surf = cv2.SURF(value)
+			kp, des = surf.detectAndCompute(gray, None)
+		
+		
+
+		bow.add(des)
+
+		if not i%10:
+			print(i, 'files done')
+		
+		i += 1
 
 		#img = cv2.drawKeypoints(gray, kp, None, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 		#plt.imshow(img)
-		
-		kp, des = sift.detectAndCompute(gray,None)
-
-		bow.add(des)
-		
 		#print(len(kp), len(des))
 		#cv2.imwrite('/home/dawid/sift_keypoints.jpg',des[3])
 
@@ -55,31 +82,41 @@ def create_bow(directory, df):
 if __name__ == "__main__":
 	
 	datasets_path = ('./by_style/', './by_country/', './by_product/' )
-	folder_name = 'bag_of_words'
-	
-	new_paths = create_dirs(datasets_path, folder_name)
+	folder_names = ['swift_bag_of_words', 'surf_bag_of_words']
+	new_paths = []
+
+	for folder in folder_names:
+		new_paths.append(create_dirs(datasets_path, folder))
 
 	print("Start processing...")
+
+	for j, folder in enumerate(folder_names):
+
+		print('Processing ', folder)
+
+		algorithm = Algorithm.SWIFT if j == 0 else Algorithm.SURF
 	
-	for i, path in enumerate(datasets_path):
+		for i, path in enumerate(datasets_path):
 
-		print("Processing %s data" % path)
+			print("Processing %s data" % path)
 
-		for data_type in ['training', 'testing']:
-			
-			data = pd.read_csv(os.path.join(path, '%s.csv' % data_type), sep=',', \
-				names = ["file", "label"])
+			for data_type in ['training', 'testing']:
 
-			print("%s data" % data_type, data.shape, 'size')
+				current_path = new_paths[j][i]
+				
+				data = pd.read_csv(os.path.join(path, '%s.csv' % data_type), sep=',', \
+					names = ["file", "label"])
 
-			data_path = os.path.join(path, data_type)
+				print("%s data" % data_type, data.shape, 'size')
 
-			training_data, labels = create_bow(data_path, data)
+				data_path = os.path.join(path, data_type)
 
-			histogram_dir = os.path.join(new_paths[i], '%s_histograms_256.dat' % data_type)
-			labels_dir = os.path.join(new_paths[i], '%s_labels_256.dat' % data_type)
+				training_data, labels = create_bow(data_path, data, algorithm)
 
-			pickle.dump(training_data, open(histogram_dir, "wb" ))
-			pickle.dump(labels, open(labels_dir, "wb" ))
+				histogram_dir = os.path.join(current_path, '%s_histograms_256.dat' % data_type)
+				labels_dir = os.path.join(current_path, '%s_labels_256.dat' % data_type)
+
+				pickle.dump(training_data, open(histogram_dir, "wb" ))
+				pickle.dump(labels, open(labels_dir, "wb" ))
 
 
