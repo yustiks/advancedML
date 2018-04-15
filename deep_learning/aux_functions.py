8c4931826f0ddc3a97672b4e285d572ced94c8f0
barbuta cipcigan e. (ebc1u17) <ebc1u17@soton.ac.uk>
2018-04-15 02:06:54 +0100

    confussion matrix with bug

--- deep_learning/aux_functions.py
+++ deep_learning/aux_functions.py
@@ -37,7 +37,7 @@ def display_confussion_matrix(conf_matrix, problem_type):
 	else:
 		return
 
-	print("")
+	print("\n")
 	print("{:^11}".format(" "), end="")
 	for e in labels_dict.values():
 		print("|{:^11}".format(e), end="")
@@ -49,10 +49,10 @@ def display_confussion_matrix(conf_matrix, problem_type):
 			print("|{:^11}".format(conf_matrix[t, p]), end="")
 		print("")
 
-	plt.title("Confussion Heat Map")
 	plt.imshow(conf_matrix, cmap='gray')
 	plt.xticks(list(labels_dict.keys()), list(labels_dict.values()), rotation=45)
 	plt.yticks(list(labels_dict.keys()), list(labels_dict.values()))
 	[x.set_color("yellow") for x in plt.gca().get_xticklabels()]
+	[y.set_color("yellow") for y in plt.gca().get_yticklabels()]
 	plt.show()
 
